62e50c470b7aa22884dfdce914d9e1fa71949409
Truman Proctor <trumanproctor@Trumans-MacBook-Pro.local>
2018-11-27 20:19:18 -0700

    React-1-mini project

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      friends: [],
+      picture: '',
+      name: ''
+    };
+  }
+
+  updatePicture( value ){
+    this.setState({ picture: value });
+  }
+
+  updateName( value ){
+    this.setState({ name: value });
+  }
+
+  addFriend() {
+    const { friends, picture, name } = this.state;
+
+    let newFriends = friends.slice();
+    newFriends.push({ picture, name });
+
+    this.setState({ friends: newFriends, picture: '', name: '' });
+  }
+
+  render() {
+    const friends = this.state.friends.map( ( friend, index ) => (
+      <div key={ `friend-${ index }-${ friend.name }` }>
+        <img width="100px" alt="" src={ friend.picture } />
+        <span>{ friend.name }</span>
+      </div>
+    ));
+
+    return (
+      <div>
+        <span>Picture:</span>
+        <input onChange={ ( e ) => this.updatePicture( e.target.value ) } value={ this.state.picture } />
+
+        <span>Name:</span>
+        <input onChange={ ( e ) => this.updateName( e.target.value ) } value={ this.state.name } />
+
+        <button onClick={ () => this.addFriend() }>Add Friend</button>
+
+        { friends }
+      </div>
+    );
+  }
+}
+
+export default App;
